aec80ddb8681c091762cd745ac03c4218a6db011
AfterWaLifu <maximaphonin@gmail.com>
2021-01-24 18:14:52 +0300

    added a few pictures

--- MonopolyGameWF/Form1.Designer.cs
+++ MonopolyGameWF/Form1.Designer.cs
@@ -29,15 +29,20 @@ namespace MonopolyGameWF
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::MonopolyGameWF.Properties.Resources.og_og_1449047923244563434;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "MG";
+            this.Text = "Monopoly The Game";
             this.ResumeLayout(false);
 
         }
