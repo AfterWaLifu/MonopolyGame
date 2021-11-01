97f8105af3e5480d2f27a1c473a6d7a1a28662b8
AfterWaLifu <maximaphonin@gmail.com>
2021-11-01 23:36:54 +0300

    поменял шрифт

--- QMonopoly/mainwindow.cpp
+++ QMonopoly/mainwindow.cpp
@@ -3,9 +3,9 @@
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
-    this->setGeometry(10,100,1600,900);
-    this->setMaximumSize(1600,900);
-    this->setMinimumSize(1600,900);
+    this->setGeometry(10,100,1200,800);
+    this->setMaximumSize(1200,800);
+    this->setMinimumSize(1200,800);
 
     init();
 }
@@ -15,16 +15,46 @@ MainWindow::~MainWindow()
     diceButton->~QPushButton();
     helpButton->~QPushButton();
     setsButton->~QPushButton();
+
+    for (int i = 0; i < 36 ; i++){
+        squares[i]->~QPushButton();
+    }
 }
 
 void MainWindow::init()
 {
+    QFont font("Verdana", 12);
+
     diceButton = new QPushButton("Бросить куб",this);
-    diceButton->setGeometry( 700 , 400 , 200 , 50 );
+    diceButton->setGeometry( 500 , 350 , 200 , 50 );
+    diceButton->setFont(font);
 
     helpButton = new QPushButton("Помощь", this);
-    helpButton->setGeometry( 700 , 450 , 100 , 50 );
+    helpButton->setGeometry( 500 , 400 , 100 , 50 );
+    helpButton->setFont(font);
 
     setsButton = new QPushButton("Настройки", this);
-    setsButton->setGeometry( 800 , 450 , 100 , 50 );
+    setsButton->setGeometry( 600 , 400 , 100 , 50 );
+    setsButton->setFont(font);
+
+    for ( int i = 0 ; i < 12 ; i++ ){
+        //горизонталь верх
+        squares[i] = new QPushButton( QString::number(i) ,this);
+        squares[i]->setGeometry(i*100, 0, 100,100);
+        squares[i]->setFont(font);
+        //горизонталь низ
+        squares[i+18] = new QPushButton( QString::number(i+18) , this);
+        squares[i+18]->setGeometry(1100-i*100 , 700, 100,100);
+        squares[i+18]->setFont(font);
+    }
+    for ( int i = 12 ; i < 18 ; i++ ){
+        //вертикаль право
+        squares[i] = new QPushButton( QString::number(i) , this);
+        squares[i]->setGeometry(1100, (i-11)*100, 100,100);
+        squares[i]->setFont(font);
+        //вертикаль лево
+        squares[i+18] = new QPushButton( QString::number(i+18) , this);
+        squares[i+18]->setGeometry(0, 700-(i-11)*100, 100,100);
+        squares[i+18]->setFont(font);
+    }
 }
--- QMonopoly/mainwindow.h
+++ QMonopoly/mainwindow.h
@@ -6,11 +6,16 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QString>
+#include <QFont>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/*!
+ * \brief The MainWindow класс основного окна программы
+ */
 class MainWindow : public QWidget
 {
     Q_OBJECT
@@ -23,10 +28,13 @@ private:
     QPushButton *diceButton;
     QPushButton *helpButton;
     QPushButton *setsButton;
-    QPushButton squares[36];
-    QPushButton buyButtons[4];
-    QPushButton sellButtons[4];
+    QPushButton *squares[36];
+    QPushButton *buyButtons[4];
+    QPushButton *sellButtons[4];
 
+    /*!
+     * \brief init Функция инициализации основного окна
+     */
     void init();
 };
 #endif // MAINWINDOW_H
